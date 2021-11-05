4399dff2e8c6cb03d801a1c8afab421ad9a716c0
Roberto West <roberto.west@gmail.com>
2021-11-05 16:20:58 -0300

    modificado por Roberto

--- webserver/html/mysql/index.php
+++ webserver/html/mysql/index.php
@@ -1,30 +1,34 @@
 <?php
 /*
-MySQLi es una opción mas para la conexión he interacción con MySQL. 
+MySQLi es una opci&oacute;n mas para la conexi&oacute;n he interacci&oacute;n con MySQL. 
 Su nombre viene de "MySQL Improved" y es una interfaz mejorada de PHP, 
 por lo que se utiliza únicamente para PHP Scripting
 */
 
-function Conectarse()
-{
-    # $host='172.29.0.2:3306';
-    $host='mysql';
-    $usuariodb='root';
-    $passwdb='roberto';
-    $nombredb='sys';
+$host='mariadb';
+$usuariodb='root';
+$passwdb='roberto';
+$nombredb='idiomagic';
 
-    $link = mysqli_connect($host, $usuariodb, $passwdb, $nombredb);
-    if (!$link) {
-        echo "Error: Incapaz de conectarse a MySQL." . PHP_EOL . "</br>";
-        echo "Depuración errno: " . mysqli_connect_errno() . PHP_EOL. "</br>";
-        echo "Depuración error: " . mysqli_connect_error() . PHP_EOL. "</br>";
-        exit;
-    }
-    return $link;
+echo "<h1>Probando conexi&oacuten pdo</h1>";
+try {
+    $link = new PDO("mysql:host=$host;dbname=$nombredb", $usuariodb, $passwdb);
+    $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexi&oacute;n exitosa !!!.";
+}
+catch(PDOException $e) {
+    echo "Error de conexi&oacute: " . $e->getMessage();
 }
 
-$link = Conectarse();
-echo "Conexión exitosa !!!." . PHP_EOL. "</br>";
-echo "Información del host: " . mysqli_get_host_info($link) . PHP_EOL;
-mysqli_close($link);
+echo "<h1>Probando conexi&oacuten mysqli</h1>";
+$link = mysqli_connect($host, $usuariodb, $passwdb, $nombredb);
+if ($link) {
+    echo "Conexi&oacute;n exitosa !!!." . PHP_EOL. "</br>";
+    echo "Informaci&oacute;n del host: " . mysqli_get_host_info($link) . PHP_EOL;
+    mysqli_close($link);
+} else {
+    echo "Error: Incapaz de conectarse a MySQL." . PHP_EOL . "</br>";
+    echo "Depuraci&oacute;n errno: " . mysqli_connect_errno() . PHP_EOL. "</br>";
+    echo "Depuraci&oacute;n error: " . mysqli_connect_error() . PHP_EOL. "</br>";
+}
 ?>
